358ae2636e5e8ab4c7d669255980ec6c210c9eec
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-02-25 00:12:23 -0800

    properly clean up files
    yems

---
+++ purgecss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['_site/**.html'],
+  css: ['assets/css/**.css'],
+  variables: true,
+  safelist: [ "fa-tags", "fa-folder-open", "fa-x-twitter", "fa-screwdriver",
+    "fa-book", "fa-chalkboard-user", "fa-facebook", "fa-linkedin", "fa-bluesky", "btn--x" ]
+}
