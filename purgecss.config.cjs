86d504030e34e7e913c5c92cbeb452660656dbd3
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-04 17:51:55 -0800

    Fix purgecss fucking up

--- purgecss.config.cjs
+++ purgecss.config.cjs
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['_site/**.html'],
-  css: ['assets/css/**.css'],
+  css: ['_site/assets/css/**.css', 'assets/css/**.css'],
   variables: true,
-  safelist: [ "fa-tags", "fa-folder-open", "fa-x-twitter", "fa-screwdriver",
-    "fa-book", "fa-chalkboard-user", "fa-facebook", "fa-linkedin", "fa-bluesky", "btn--x" ]
+  safelist: {
+    standard: ["fa-tags", "fa-copy", "far", "fas", "fa-fw",
+    "fa-check", "copied", "fa-folder-open", "fa-x-twitter", "fa-screwdriver",
+    "fa-book", "fa-chalkboard-user", "fa-facebook", "fa-linkedin",
+    "fa-bluesky", "btn--x", "btn--linkedin", "btn--facebook", "btn--bluesky",
+    "search-content", "pagination", "pagination--pager", "is--hidden", "is--visible",
+    "search-content__inner-wrap", "search-content__form", "search-input", "sr-only",
+    "results", "clipboard-copy-button", "faqcontainer"],
+    deep: [/mfp-*/]
+  }
 }
