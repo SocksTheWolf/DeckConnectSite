7ac2ea19752db07ad905df08dbe1b809ad808239
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-06 22:55:07 -0800

    update dl script
    swear to god this shit better work

--- .github/run.sh
+++ .github/run.sh
@@ -1,5 +1,3 @@
-# .  ./.gh-api-examples.conf
-
 # https://docs.github.com/en/rest/reference/repos#get-a-release-asset
 # GET /repos/{owner}/{repo}/releases/assets/{asset_id}
 
@@ -7,14 +5,14 @@ mkdir tmp
 org=socksthewolf
 repo=deckconnectbridge
 response_file=tmp/releases-latest.json
-response=$(curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/releases/latest > ${response_file} )
+response=$(curl -sH "Authorization: Bearer ${TOKEN}" https://api.github.com/repos/${org}/${repo}/releases/latest > ${response_file} )
 
 asset_id=$(cat ${response_file} | jq -r '.assets[0] .id')
 asset_name=$(cat ${response_file} | jq -r '.assets[0] .name')
-mkdir ../_site/dl
+mkdir --parents ../_site/dl
 curl ${curl_custom_flags} \
      -L \
      -H "Accept: application/octet-stream" \
-     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/releases/assets/${asset_id}" -o "_site/dl/${asset_name}"
+     -H "Authorization: Bearer ${TOKEN}" \
+        "https://api.github.com/repos/${org}/${repo}/releases/assets/${asset_id}" -o "_site/dl/${asset_name}"
 
