fe35d63ee4785696029fd00b4373557102a22364
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-06 22:44:30 -0800

    I'm so tired of this shit

---
+++ .github/run.sh
@@ -0,0 +1,20 @@
+# .  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/repos#get-a-release-asset
+# GET /repos/{owner}/{repo}/releases/assets/{asset_id}
+
+org=socksthewolf
+repo=deckconnectbridge
+response_file=tmp/releases-latest.json
+response=$(curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/releases/latest > ${response_file} )
+
+asset_id=$(cat ${response_file} | jq -r '.assets[0] .id')
+asset_name=$(cat ${response_file} | jq -r '.assets[0] .name')
+cd ..
+mkdir _site/dl
+curl ${curl_custom_flags} \
+     -L \
+     -H "Accept: application/octet-stream" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/releases/assets/${asset_id}" -o "_site/dl/${asset_name}"
+
